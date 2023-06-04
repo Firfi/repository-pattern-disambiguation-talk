03515bb32d3803c5746b940f1c97548ca01f26d7
Igor Loskutov <igor.loskutoff@gmail.com>
2023-06-04 10:48:18 +0700

    Initial commit

---
+++ apps/repository-pattern-disambiguation/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'repository-pattern-disambiguation',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/repository-pattern-disambiguation',
+};
